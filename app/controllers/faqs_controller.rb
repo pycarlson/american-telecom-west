7194fd64f1f476d4614c4226201dec5a64133d0f
Paulette Luftig <pyluftig@gmail.com>
2014-05-07 19:28:47 -0700

    added object creation forms

--- app/controllers/faqs_controller.rb
+++ app/controllers/faqs_controller.rb
@@ -1,26 +1,36 @@
 class FaqsController < ApplicationController
   
- def index 
+  def index 
+    @faqs = Faq.all
   end
 
   def new
+    @faq = Faq.new
   end
 
   def create
+    @faq = Faq.create!(faq_params)
   end
 
   def edit
+    @faq = Faq.find(params[:id])
   end
 
   def update
+
   end
 
   def destroy
+    Faq.find(params[:id])
+    respond_to do |format|
+      format.html { redirect_to faqs_path }
+      format.js
+    end
   end
 
   private
 
-  def faqs_params
+  def faq_params
     params.require(:faqs).permit(:question, :answer)
   end
 
